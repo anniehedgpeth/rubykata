49608659e0bbe611ee874e358ad2f02aca67aaa1
anniehedgpeth <annie@Annies-MBP.tx.rr.com>
2017-02-21 08:40:49 -0600

    #10

--- main.rb
+++ main.rb
@@ -1,27 +1,38 @@
+class Person
+
+  def initialize(name, employee_number, hobbies)
+    @name = name
+    @employee_number = employee_number
+    @hobbies = hobbies
+  end
+
+  def greet
+    puts "Hi, #{@name}. Your employee number is: #{@employee_number}"
+    if @name =~ /Ann/
+      puts 'Your name is alot like the creator of this program.'
+    end
+  end
+
+  def comment_on_hobbies
+    if @hobbies.count > 5
+      puts 'You have too much going on, and you need to get rid of something.'
+    elsif @hobbies.count.zero?
+      puts 'You need to learn something.'
+    end
+  end
+
+end
+
+# This calls the initialize method because I'm "initializing" a new person in that Class - creating a "Person" object.
 my_name = 'Annie'
 my_employee_number = 7
-
-def greet(name, employee_number)
-  puts "Hi, #{name}. Your employee number is: #{employee_number}"
-  if name =~ /Ann/
-    puts 'Your name is alot like the creator of this program.'
-  end
-end
-
-greet my_name, my_employee_number
-
 my_hobbies = ['yoga', 'running', 'cooking', 'singing', 'decorating', 'building']
 
-your_hobbies = []
+me = Person.new my_name, my_employee_number, my_hobbies
+you = Person.new 'Michael', 8, []
 
-# hobbies is a parameter into the method. So when you pass in my_hobbies, you're passing in that variable as the parameter.
-def comment_on_hobbies(hobbies)
-  if hobbies.count > 5
-    puts 'You have too much going on, and you need to get rid of something.'
-  elsif hobbies.count.zero?
-    puts 'You need to learn something.'
-  end
-end
+me.greet
+you.greet
 
-comment_on_hobbies my_hobbies
-comment_on_hobbies your_hobbies
+me.comment_on_hobbies
+you.comment_on_hobbies
