675c519b34bbff2bbd135d687843012138db49eb
anniehedgpeth <annie@Annies-MBP.tx.rr.com>
2017-02-21 07:58:03 -0600

    #5

--- main.rb
+++ main.rb
@@ -3,4 +3,8 @@ employee_number = 7
 
 puts "Hi, #{name}. Your employee number is: #{employee_number}"
 
-hobbies = ['yoga', 'running', 'cooking', 'singing']
+hobbies = ['yoga', 'running', 'cooking', 'singing', 'decorating', 'building']
+
+if hobbies.count > 5
+  puts 'You have too much going on, and you need to get rid of something.'
+end
